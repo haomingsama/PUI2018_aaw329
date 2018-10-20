f6c6aec55ec0ec0ac31ab60ae88bf51f986b38eb
Aric Werner <aaw329@nyu.edu>
2018-10-20 16:56:08 -0400

    Add my own helper script

---
+++ HW6_aaw329/get_citibike_data.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+url_base = "https://s3.amazonaws.com/tripdata"
+
+
+def get_citibike_data(filename):
+    """
+    Download Citi Bike data into PUIDATA directory if it's not there yet
+
+    Takes filename as argument. For example, "201809-citibike-tripdata.csv".
+    """
+
+    # Check if the data is not there in .csv or .zip format
+    if not os.path.isfile(puidata + "/" + filename) 
+          and not os.path.isfile(puidata + "/" + filename + ".zip"):
+        print("Downloading data")
+        os.system("wget -O " + puidata + "/" + filename + ".zip" + " " + 
+                  url_base + "/" + filename + ".zip")
+        print("Unzipping data")
+        os.system("unzip -d " + puidata + " " + 
+                  puidata + "/" + filename + ".zip")
+    # Check if the data _is_ there .zip format but not .csv, 
+    # in which case we just need to unzip it
+    elif os.path.isfile(puidata + "/" + filename + ".zip") 
+          and not os.path.isfile(puidata + "/" + filename):
+        print("Unzipping data")
+        os.system("unzip -d " + puidata + " " + 
+                  puidata + "/" + filename + ".zip")
+
+    # Finally, make sure the correct file is present
+    if not os.path.isfile(puidata + "/" + filename):
+        print("Problem acquiring data!")
+        sys.exit(1)
+    else:
+        print("Successfully acquired data")
